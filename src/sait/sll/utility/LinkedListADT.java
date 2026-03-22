889e413a160314b05aaa447b3de8f5795b7ac5c1
fega El-pupu <fega.eze18@gmail.com>
2026-03-22 12:18:33 -0600

    Refactor LinkedListADT interface documentation

--- src/sait/sll/utility/LinkedListADT.java
+++ src/sait/sll/utility/LinkedListADT.java
@@ -1,17 +1,8 @@
 package sait.sll.utility;
 
-/**
- * Abstract data type for a linked list.
- * 
- * @version 2025
- *
- */
 public interface LinkedListADT 
 {
-	/**
-	 * Returns true if this list is empty, otherwise false.
-	 * @return true if list is empty, false otherwise.
-	 */
+	
 	boolean isEmpty();
 
 	/**
@@ -20,7 +11,6 @@ public interface LinkedListADT
 	void clear();
 	
 	/**
-	 * Appends element to the end of this list.
 	 * @param data data to append.
 	 */
 	void append(Object data);
@@ -32,8 +22,6 @@ public interface LinkedListADT
 	void prepend(Object data);
 	
 	/**
-	 * Inserts element at a specific index of this list.
-	 * @param data data to insert.
 	 * @param index index to insert data at.
 	 * @exception IndexOutOfBoundsException Thrown if index is negative or past the size of the list.
 	 */
@@ -41,8 +29,6 @@ public interface LinkedListADT
 
 	/**
 	 * Replaces the element at a specific index of this list.
-	 * @param data data to replace.
-	 * @param index index of element to replace.
 	 * @exception IndexOutOfBoundsException Thrown if index is negative or larger than size - 1 of list.
 	 */
 	void replace(Object data, int index) throws IndexOutOfBoundsException;
@@ -54,30 +40,24 @@ public interface LinkedListADT
 	int size();
 
 	/**
-	 * Removes an element at the specific index from this list.
-	 * @param index index of the element to remove.
 	 * @exception IndexOutOfBoundsException Thrown if index is negative or larger than size - 1 of list.
 	 */
 	void delete(int index) throws IndexOutOfBoundsException;
 	
 	/**
 	 * Returns the data of the element at the specific index of this list.
-	 * @param index index of element to get.
-	 * @return data in element with matching index.
 	 * @exception IndexOutOfBoundsException Thrown if index is negative or larger than size - 1 of list.
 	 */
 	Object retrieve(int index) throws IndexOutOfBoundsException;
 
 	/**
 	 * Returns the index of the first occurrence of the element with the specific data in this list.
-	 * @param data data of the element to find the first index of.
 	 * @return index of the first element with matching data, -1 if not found.
 	 */
 	int indexOf(Object data);
 
 	/**
 	 * Returns true if this list contains an element with specified data, otherwise false.
-	 * @param data data to search for.
 	 * @return true if element containing data exists, false otherwise.
 	 */
 	boolean contains(Object data);
